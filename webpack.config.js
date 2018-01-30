826cdf197f5211dff890309460d9fe2d0090c93b
Munjal <Kavish.Munjal@cerner.com>
2018-01-29 21:37:37 -0600

    Production Deployment Best Practices

--- webpack.config.js
+++ webpack.config.js
@@ -1,4 +1,5 @@
 const path = require('path');
+const webpack = require('webpack');
 
 const config = {
   resolve: {
@@ -7,20 +8,41 @@ const config = {
       path.resolve('./node_modules'),
     ]
   },
-  entry: ['babel-polyfill', 'renderers/dom.js'],
+  entry: {
+    vendor: [
+      'babel-polyfill',
+      'react',
+      'react-dom',
+      'axios',
+      'prop-types',
+      'lodash.debounce',
+      'lodash.pickby'
+    ],
+    app: ['renderers/dom.js']
+  },
   output: {
     path: path.resolve(__dirname, 'public'),
-    filename: 'bundle.js'
+    filename: '[name].js'
   },
   module: {
     rules: [
       {
         test: /\.(js|jsx)$/,
-        use: 'babel-loader',
+        use: {
+          loader: 'babel-loader',
+          options: {
+            presets: ['react', 'env', 'stage-2']
+          }
+        },
         exclude: /node_modules/
       }
     ]
-  }
+  },
+  plugins: [
+    new webpack.optimize.CommonsChunkPlugin({
+      name: 'vendor',
+    })
+  ]
 };
 
 module.exports = config;
